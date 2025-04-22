d7f3e254fc58c191c8dd957058d1763b325cd6cc
Periklis Fregkos <fregkos@gmail.com>
2025-04-23 00:12:12 +0300

    NANANANANANANA

--- spark/spark-single.py
+++ spark/spark-single.py
@@ -6,26 +6,22 @@ sc = SparkContext(appName="ShuffleTrafficTest")
 
 # Test
 n = 1_000
-rdd = sc.parallelize(range(1,n+1))
+rdd = sc.parallelize(range(1, n + 1), n)
 
-def emit_pairs(partition_index, iterator):
-    data = list(iterator)
+
+def emit_pairs(data: iter):
     result = []
     for i in data:
-        for j in range(1,n+1):
+        for j in range(1, n + 1):
             if i != j:
-                result.append(((i,j), i))
-    return result 
+                key = tuple(sorted((i, j)))
+                result.append((key, i))
+    return result
 
-pairs_rdd = rdd.mapPartitionsWithIndex(emit_pairs)
 
-pair_lists_rdd = pairs_rdd.map(lambda kv: (kv[0], [kv[1]]))
+pairs_rdd = rdd.mapPartitions(emit_pairs).groupByKey().mapValues(list)
 
-reduced_rdd = pair_lists_rdd.reduceByKey(lambda a,b: a + b)
-
-final_rdd = reduced_rdd.map(lambda kv: (kv[0], sorted(set(kv[1] + [kv[0][1]]))))
-
-print(final_rdd.take(10))
+print(pairs_rdd.take(20))
 
 # Test case 1: [key, (value)]
 """mapped1 = rdd.map(lambda x: (x % 100, x))
