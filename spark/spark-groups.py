f442f3122a9e2b3aa6da273b5fd3fcf4490e5f88
Lazaros Gogos <lazarosg@csd.auth.gr>
2025-05-08 22:27:31 +0300

    one step closer to achieving spark AGI

--- spark/spark-groups.py
+++ spark/spark-groups.py
@@ -10,11 +10,11 @@ sc = SparkContext(appName="GroupedShuffleTrafficTest")
 
 n = 3000
 group_size = 100
+payload = b"a" * (1024 * 1024)
+payload = len(payload)  # 1048576
 
 # Step 1: Predefine groups
 groups = [list(range(i, i + group_size)) for i in range(1, n + 1, group_size)]
-# print(f"{groups=}")
-# pprint(groups)
 
 # Step 2: Create group pairs (with self-pairs included)
 # Result: [ ((1, group1_100), (2, group101_200)), ((1, group1_100)), (3, group201_300)), ... ]
@@ -23,30 +23,27 @@ group_pairs = list(combinations_with_replacement(enumerate(groups, 1), 2))
 
 # Step 3: Parallelize group pairs (one per partition)
 group_pair_rdd = sc.parallelize(group_pairs, len(groups))
-# group_pair_rdd = sc.parallelize(group_pairs, len(groups)).glom()
-# pprint(group_pair_rdd.collect(), width=120)
-# pprint(len(group_pair_rdd.collect()), width=120)
-# sc.stop()
+
 
 # Step 4: Define logic to emit all (i, j) → [i, j] from two groups
+data_exchanged = sc.accumulator(0)
+
+
 def emit_pairwise_records(group_pair):
     (id1, g1), (id2, g2) = group_pair
-    # output = []
+    l1 = len(g1)
+    l2 = len(g2)
+    
+    data_exchanged.add(l1 + l2)
     for i in g1:
         for j in g2:
-            # output.append(((i, j), [i, j]))
-            yield ((i, j), "a" * 1024 ** 2)
-
-            # yield ((i, j), [i, j])
-
+            yield ((i, j), 1)
 
 # Step 5: Apply the mapping logic
 result_rdd = group_pair_rdd.flatMap(emit_pairwise_records).groupByKey().mapValues(list)
-# print(result_rdd.collect(), width=120)
-# pprint(len(result_rdd.collect()), width=120)
 
 # (Optional) Inspect or save result
-print(result_rdd.take(10))
-# result_rdd.collect()
+result_rdd.take(1000)
 
+print("DATA_EXCHANGED", data_exchanged.value)
 sc.stop()
--- spark/spark-single.py
+++ spark/spark-single.py
@@ -7,13 +7,20 @@ import sys
 logging.basicConfig(level=logging.INFO)
 sc = SparkContext(appName="ShuffleTrafficTest")
 
-# Test
-n = 1_000
+n = 3000
 rdd = sc.parallelize(range(1, n + 1), n)
 
+# n = 3000
+# data_exchanged = 18_868_814_298_000
+
+data_exchanged = sc.accumulator(0)
+
+payload = b"a" * (1024 * 1024)
+payload = len(payload)  # 1048576
 
 def emit_pairs(data: iter):
     result = []
+    data_exchanged.add(1) # one for i, one for j
     for i in data:
         for j in range(1, n + 1):
             if i != j:
@@ -26,37 +33,18 @@ def emit_dummy_payload(data: iter):
     for i in data:
         for j in range(1, n + 1):
             if i != j:
+                data_exchanged.add(1)  # one for i, one for j
+                # is this valid? should it be 1 maybe ?
                 key = tuple(sorted((i, j)))
-                # result.append((key, "dummy payload of size 100 MB".encode('utf-8') * 125000))``
-                # yield (key, "dummy payload of size 100 MB".encode('utf-8') * 1250)
-                yield (key, "a" * 1024 ** 2)
+                yield (key, 1)
+
 
 pairs_rdd = rdd.mapPartitions(emit_dummy_payload).groupByKey().mapValues(list)
 
-# print(pairs_rdd.take(5))
+pairs_rdd.take(1000)
+print("DATA_EXCHANGED", data_exchanged.value)
 
 
-# remove the whole directory first
-if os.path.exists("pairs_output"):
-    shutil.rmtree("pairs_output")
-
-pairs_rdd.saveAsTextFile("pairs_output")
-
-
-# Test case 1: [key, (value)]
-"""mapped1 = rdd.map(lambda x: (x % 100, x))
-reduced1 = mapped1.groupByKey().mapValues(sum)
-
-results = reduced1.collect()
-logging.info("Count of reduced1: %s", len(results))
-logging.info(f"Results:  {str(results)}")
-
-# Test case 2: [key, (v1, v2, v3)]
-mapped2 = rdd.map(lambda x: (x % 100, (x, x*2, x*3)))
-reduced2 = mapped2.groupByKey().mapValues(lambda vals: sum(a+b+c for (a,b,c) in vals))
-# reduced2.count()
-results = reduced2.collect()
-logging.info("Count of reduced2: %s", len(results))
-logging.info(f"Results: {str(results)}")
-"""
 sc.stop()
+# single: 8997000 iterations
+# groups:   93000 iterations
